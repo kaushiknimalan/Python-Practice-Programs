b826ee339e3e56d12f5b35fc9742fb721d5ee6d1
Kaushik Nimalan <66821715+kaushiknimalan@users.noreply.github.com>
2021-05-10 17:41:06 +0530

    Committed the code

---
+++ Strings/Checking if 2 strings have the same characters/main.py
@@ -0,0 +1,16 @@
+wrd1 = input('Your 1st Word Here: ')
+wrd2 = input('Your 2nd Word Here: ')
+wrd1 = wrd1.lower()
+wrd2 = wrd2.lower()
+is_there = 0
+is_not_there = 0
+
+for letter in wrd2:
+    if letter in wrd1:
+        is_there += 1
+    else:
+        is_not_there += 1
+if not is_not_there >= 1:
+    print('True')
+else:
+    print('False')
