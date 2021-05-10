ea57c5d8933caf093c972ea4582c2e4801821b64
Kaushik Nimalan <66821715+kaushiknimalan@users.noreply.github.com>
2021-05-10 16:28:30 +0530

    Changed the code

--- Strings/Delete_Repeating_Letters/Delete_repeating_letters.py
+++ Strings/Delete_Repeating_Letters/Delete_repeating_letters.py
@@ -4,13 +4,13 @@ index = 0
 while index <= len(word) - 1:
     ind = 0
     r_val = 0
-    while ind < len(word) - 1:
+
+    while ind <= len(word) - 1:
         if word[ind] == word[index]:
             r_val += 1
         ind += 1
 
-    if not r_val == 1 or r_val == 0:
-        blank_space = ""
-        word.replace(word[index], blank_space)
+    if not r_val == 1:
+        word = word.replace(word[index], "")
     index += 1
 print(word)
